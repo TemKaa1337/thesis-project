53bcddd148f6acca6268b52f3b2e0b0d9bea80df
argentum1 <artem_komarov@argentum-ks.com>
2020-03-28 16:58:58 +0300

    adding header base css

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,98 +6,37 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
         <link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
         <script src="{{ asset('js/main.js') }}" type="text/javascript"></script>
 
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <header>
+            <div class = "container">
+                <a href = "/" class = "header-logo">LOGO</a>
+                <nav>
+                    <ul>
+                        <li><a href = "mainpage">Main page</a></li>
+                        <li><a href = "mainpage">Cinema ticket booking</a></li>
+                        <li><a href = "mainpage">Contacts</a></li>
+                        <li><a href = "mainpage">About us</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div class = "main">
+            <div class = "container">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
+        <footer>
+            <div class = "container">
+
+            </div>
+        </footer>
     </body>
 </html>
